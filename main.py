a2d0f8c4e624265c78b481135b0b1b461a4ed5f2
caeljandcristobal <ccaeljan@gmail.com>
2026-09-04 22:59:49 +0800

    fix: fix mismatch in import name

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import os
 from dotenv import load_dotenv
-import requests
+from n2yo import N2YO
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-url = f"https://api.n2yo.com/rest/v1/satellite/tle/45217/&apiKey={API_KEY}"
+n2yo = N2YO(API_KEY)
 
-response = requests.get(url)
-data = response.json()
+data = n2yo.get_tle(45217)
 
-satellite_name = data["info"]["satname"]
-satellite_id = data["info"]["satid"]
-
-tle = data["tle"]
-
-print("Satellite:", satellite_name)
-print("NORAD ID:", satellite_id)
-print("TLE:")
-print(tle)
+print(data["info"]["satname"])
+print(data["tle"])
---
+++ n2yo.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class N2YO:
+    BASE_URL = "https://api.n2yo.com/rest/v1/satellite"
+
+    # constructor for N2YO object
+    def __init__(self, api_key):
+        # stores api key from .env file
+        self.api_key = api_key
+
+    # method to get tle data
+    def get_tle(self, satellite_id):
+        # formatted url including satellite id
+        url = f"{self.BASE_URL}/tle/{satellite_id}"
+
+        # struct of api key
+        params = {
+            "apiKey": self.api_key
+        }
+
+        # response request to get tle data
+        response = requests.get(url, params=params)
+
+        # check status
+        response.raise_for_status()
+
+        # return tle data in json form
+        return response.json()
