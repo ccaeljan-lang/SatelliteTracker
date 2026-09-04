5a1980d2a57063c6afe593c34a1e5b12d15e2796
caeljandcristobal <ccaeljan@gmail.com>
2026-09-04 22:12:03 +0800

    feat: add satellite API client

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
+from dotenv import load_dotenv
 import requests
 
-API_KEY = "YOUR_API_KEY"
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+url = f"https://api.n2yo.com/rest/v1/satellite/tle/45217/&apiKey={API_KEY}"
+
+response = requests.get(url)
+data = response.json()
+
+satellite_name = data["info"]["satname"]
+satellite_id = data["info"]["satid"]
+
+tle = data["tle"]
+
+print("Satellite:", satellite_name)
+print("NORAD ID:", satellite_id)
+print("TLE:")
+print(tle)
